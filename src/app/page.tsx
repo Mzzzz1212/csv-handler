7e0642eeb9ad2e67ad6ffcf93e838edfb8ec860d
Mzzzz1212 <mahizhsiva@gmail.com>
2025-09-25 19:52:10 +0530

    4th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ import { useCallback, useMemo, useState } from "react";
 import { toast } from "@/hooks/use-toast";
 
 interface BookRecord {
+  id: string;
   Title: string;
   Author: string;
   Genre: string;
